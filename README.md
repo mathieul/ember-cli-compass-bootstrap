fa8aba82003818a7cbe639dc8084b423e44106f9
Mathieu Lajugie <mathieu@caring.com>
2014-07-11 21:53:12 -0700

    replace node-sass includePaths option with using broccoli trees

---
+++ README.md
@@ -0,0 +1,30 @@
+ember-cli-compass-bootstrap
+===========================
+
+This is an addon for [ember-cli](http://iamstef.net/ember-cli/) that adds support for:
+
+  * [Compass](http://compass-style.org) (through bower package [compass-mixins](https://github.com/Igosuki/compass-mixins))
+  * [Bootstrap for Sass](http://getbootstrap.com)
+
+The package is using libsass (coming with ember-cli through broccoli-sass and node-sass) to benefit from
+ultra fast preprocessing. Compass and Bootstrap for Sass are installed using bower.
+
+## Usage
+
+It is a two-steps installation process. You install the npm module, ember-cli will automatically
+pick up the addon. You also need to generate a scss file, which will also update npm and bower
+configuration files with the necessary dependencies.
+
+First install the npm package:
+
+```
+npm install ember-cli-compass-bootstrap --save-dev
+```
+
+Then generate a new app.scss file and delete the old `app.css` file (after copying
+any custom content to the new `app.scss`):
+
+```
+ember g scss app
+rm app/styles/app.css
+```
--- index.js
+++ index.js
@@ -1,36 +1,6 @@
 'use strict';
 
-function merge() {
-  var params = [].slice.apply(arguments),
-      destination = params.shift();
-
-  for (var i = 0, len = params.length; i < len; i += 1) {
-    if (!params[i]) { continue; }
-    for (var name in params[i]) {
-      if (params[i].hasOwnProperty(name)) {
-        destination[name] = params[i][name];
-      }
-    }
-  }
-
-  return destination;
-}
-
-function _setSassOptions(app) {
-  var plugins = app.registry.registry.css,
-      index, plugin;
-
-  for (index in plugins) {
-    plugin = plugins[index];
-    if (plugin.name === 'broccoli-sass') {
-      plugin.options.includePaths = [
-        'vendor/bootstrap-sass-official/assets/stylesheets',
-        'vendor/compass-mixins/lib'
-      ];
-      console.log("Sass options updated.");
-    }
-  }
-}
+var mergeTrees = require('broccoli-merge-trees');
 
 function CompassBootstrapAddon(project) {
   this.project = project;
@@ -41,8 +11,13 @@ CompassBootstrapAddon.prototype.blueprintsPath = function () {
   return __dirname + '/blueprints';
 };
 
-CompassBootstrapAddon.prototype.included = function (app) {
-  _setSassOptions(app);
+CompassBootstrapAddon.prototype.treeFor = function (type) {
+  if (type !== 'styles') { return; }
+
+  return mergeTrees([
+    'vendor/bootstrap-sass-official/assets/stylesheets',
+    'vendor/compass-mixins/lib'
+  ]);
 };
 
 module.exports  = CompassBootstrapAddon;
